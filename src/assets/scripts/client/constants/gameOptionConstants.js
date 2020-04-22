0da95a1e48154908e60716dc7e6f468c7815161c
Anton <antbl294@student.liu.se>
2020-04-22 14:59:50 +0200

    test

--- src/assets/scripts/client/constants/gameOptionConstants.js
+++ src/assets/scripts/client/constants/gameOptionConstants.js
@@ -34,7 +34,7 @@ export const GAME_OPTION_VALUES = [
     {
         name: GAME_OPTION_NAMES.THEME,
         defaultValue: 'DEFAULT',
-        description: 'Soap Theme',
+        description: 'Style Theme',
         type: 'select',
         onChangeEventHandler: EVENT.SET_THEME,
         optionList: [
